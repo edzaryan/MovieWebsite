c071cd965c57d854ed52b039b7b3feacc0e44c9b
Edgar Yeghiazaryan <yeghiazaryanedgar93@gmail.com>
2026-01-24 18:49:43 +0400

    Added MultipleSelector Component

---
+++ client/src/validation/yupExtensions.ts
@@ -0,0 +1,23 @@
+import * as Yup from 'yup';
+
+export const firstLetterUppercase = (value?: string | null): boolean => {
+  if (!value) return true;
+
+  const trimmed = value.trim();
+  if (!trimmed) return true;
+
+  const firstChar = trimmed[0];
+  return firstChar === firstChar.toUpperCase();
+}
+
+Yup.addMethod(Yup.string, 'firstLetterUppercase', function (message) {
+  return this.test('first-letter-uppercase', message, function (value) {
+    const { path, createError } = this;
+
+    if (firstLetterUppercase(value)) {
+      return true;
+    }
+
+    return createError({ path, message });
+  });
+});
